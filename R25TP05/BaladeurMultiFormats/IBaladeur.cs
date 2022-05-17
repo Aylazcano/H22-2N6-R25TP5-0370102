a3e2b02c56b1f90166f2dde14ec22e3105d1f70a
Alexis Lazcano <0370102@cegepmontpetit.ca>
2022-05-17 19:21:38 -0400

    FCT : TODO #07 Ajout de l’interface IBaladeur

---
+++ R25TP05/BaladeurMultiFormats/IBaladeur.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Forms;
+
+namespace BaladeurMultiFormats
+{
+    public interface IBaladeur
+	{
+        #region PROPRIETE
+        /// <summary>
+        /// Obtient le nombre de chansons
+        /// </summary>
+        int NbChansons { get; }
+        #endregion
+
+        #region METHODES
+        /// <summary>
+        /// Affiche la liste des chansons dans la pListView passée en paramètre
+        /// </summary>
+        /// <param name="pListView">Liste des chansons</param>
+        void AfficherLesChansons(ListView pListView);
+
+		/// <summary>
+		/// Obtient la chanson à l’index pIndex passé en paramètre
+		/// </summary>
+		/// <param name="pIndex">Index de la chanson selectionnee</param>
+		/// <returns></returns>
+		Chanson ChansonAt(int pIndex);
+
+		/// <summary>
+		/// Récupère la liste des fichiers dans le dossier Chansons, instancie selon le cas des objets des classes dérivées de la classe Chanson
+		/// </summary>
+		void ConstruireLaListeDesChansons();
+
+		/// <summary>
+		/// Instancie une ChansonAAC à partir de la chanson à l’index pIndex, enregistre les paroles et supprime le fichier du format précédent.
+		/// </summary>
+		/// <param name="pIndex">Index de la chanson selectionnee</param>
+		void ConvertirVersAAC(int pIndex);
+
+		/// <summary>
+		/// Instancie une ChansonMP3 à partir de la chanson à l’index pIndex, enregistre les paroles et supprime le fichier du format précédent.
+		/// </summary>
+		/// <param name="pIndex">Index de la chanson selectionnee</param>
+		void ConvertirVersMP3(int pIndex);
+
+		/// <summary>
+		/// Instancie une ChansonWMA à partir de la chanson à l’index pIndex, enregistre les paroles et supprime le fichier du format précédent
+		/// </summary>
+		/// <param name="pIndex">Index de la chanson selectionne</param>
+		void ConvertirVersWMA(int pIndex);
+        #endregion
+    }
+}
